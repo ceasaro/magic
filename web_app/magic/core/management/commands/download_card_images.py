74d284b08ef2aa29526d63022811808e8ca687be
ceasaro <ceesvw@gmail.com>
2023-04-01 15:38:23 +0200

    download a card images of a set.

--- web_app/magic/core/management/commands/download_card_images.py
+++ web_app/magic/core/management/commands/download_card_images.py
@@ -1,11 +1,15 @@
 import os
 from django.core.management import BaseCommand
 
-from magic.core.models import Card
+from magic.core.models import Card, Set
 
 
 class Command(BaseCommand):
     help = "download card images of card"
+
+    def __init__(self, stdout=None, stderr=None, no_color=False, force_color=False):
+        super().__init__(stdout, stderr, no_color, force_color)
+        self.verbosity = None
 
     def add_arguments(self, parser):
 
@@ -13,39 +17,46 @@ class Command(BaseCommand):
         parser.add_argument(
             "--all",
             action="store_true",
-            dest="all",
             default=False,
             help="download the images for all cards (if image is not present)",
         )
         parser.add_argument(
             "-c",
             "--card",
-            dest="card",
             help="download image for this card",
         )
         parser.add_argument(
             "-r",
             "--refresh",
             action="store_true",
-            dest="refresh",
             default=False,
             help="refresh image card(s) even it has already been downloaded",
         )
+        parser.add_argument(
+            "-s",
+            "--set_name",
+            help="download all images for this set",
+        )
 
-    def handle(self, *args, **options):
-        refresh = options["refresh"]
-        if options["card"]:
-            self.download_image(
-                Card.objects.filter(name=options["card"]), refresh=refresh
-            )
-        elif options["all"]:
+    def handle(self, all=False, card=None, refresh=False, set_name=False, *args, **kwargs):
+        self.verbosity = kwargs.get("verbosity", 1)
+        if card:
+            self.download_images(Card.objects.filter(card), refresh=refresh)
+        if set_name:
+            sets = Set.objects.filter(name=set_name)
+            if sets.count() == 1:
+                card_set = sets.first()
+                cards = Card.objects.filter(set=card_set)
+                if console_confirm(f"Download all ({cards.count()}) images for set '{card_set}'?"):
+                    self.download_images(cards, refresh=refresh)
+        elif all:
             if console_confirm(
                 "Are yo sure you want to download all missing card images, this can take a while.?"
             ):
                 if refresh:
-                    self.download_image(Card.objects.all(), refresh=refresh)
+                    self.download_images(Card.objects.all(), refresh=refresh)
                 else:
-                    self.download_image(Card.objects.filter(image__isnull=not refresh))
+                    self.download_images(Card.objects.filter(image__isnull=not refresh))
             else:
                 print("Downloading card images aborted")
         else:
@@ -56,14 +67,19 @@ class Command(BaseCommand):
                 )
             )
 
-    def download_image(self, cards, refresh=False):
+    def download_images(self, cards, refresh=False):
         total = len(cards)
         for i, card in enumerate(cards):
             try:
-                card.download_image(refresh=refresh)
-                message = "downloaded image for card '{}'".format(card)
-            except:
+                card, updated = card.download_image(refresh=refresh)
+                if updated:
+                    message = "downloaded image for card '{}'".format(card)
+                else:
+                    message = "skipped image for card '{}'".format(card)
+            except Exception as e:
                 message = "ERROR downloading image for card '{}'".format(card)
+                if self.verbosity >= 2:
+                    print(f"\n\n {e}")
             print("{}/{}: {}".format(i, total, message))
 
 
